9f198540f0e3f825b206d0eddb7fc4476930b0c5
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-10 21:15:11 +0400

    server.js commit

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ const corsOptions = {
     origin:"http://localhost:3000",
     optionsSuccessStatus: 200
 }
+
+app.use(cors(corsOptions))
+// app.use(bodyParser.json()); 
+// dotenv.config();
+
+
+app.get("/", async (req, res) => {
+    console.log("Connected to Server");
+    res.send("Server is running");
+    
+});
