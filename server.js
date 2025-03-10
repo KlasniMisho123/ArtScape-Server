f328e81a49094c876dbae381161836b26c753cde
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-10 21:09:10 +0400

    server.js

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ import axios from "axios";
 import express from "express";
 import cors from "cors";
 // Server side
+
+const app = express()
+const port = 5000
+
+const corsOptions = {
+    origin:"http://localhost:3000",
+    optionsSuccessStatus: 200
+}
